2badcfbca10610e35fabd88999cb022c0b06ff76
Atul Sharma <atulanilsharma@gmail.com>
2022-12-13 13:34:38 +0530

    Efficient and Naive optimisation solution
    * Adds a parallelised efficient and naive optimisation solution
    * Adds documentation to each optimisation method
    * Updates the benchmark and simulation service handlers to support parallelisation

--- src/simulations/router.py
+++ src/simulations/router.py
@@ -1,18 +1,24 @@
+import asyncio, concurrent
 from fastapi import APIRouter
-from simulations.models import OptType, OptimizeRequestModel, BenchmarkRequestModel, Benchmark
+from simulations.models import OptType, OptimizeRequestModel, BenchmarkRequestModel, benchmark_simulation
 from utils.modulo_maximisation import naive, efficient
 
 sim_router = APIRouter()
 
 
-@sim_router.post("/benchmark/{type}")
-async def benchmark_algo(type: OptType, params: BenchmarkRequestModel):
-    return Benchmark.benchmark_simulation(type, params)
+@sim_router.post("/benchmark/{opt_type}")
+async def benchmark_algo(opt_type: OptType, params: BenchmarkRequestModel):
+    loop = asyncio.get_event_loop()
+    with concurrent.futures.ProcessPoolExecutor() as pool:
+        result = await loop.run_in_executor(
+            pool, benchmark_simulation, opt_type, params.replication, params.num_lists, params.num_elements
+        )
+    return result
 
 
-@sim_router.post("/optimize/{type}")
-async def run_algo(type: OptType, params: OptimizeRequestModel):
-    if type == OptType.naive:
+@sim_router.post("/optimize/{opt_type}")
+async def run_algo(opt_type: OptType, params: OptimizeRequestModel):
+    if opt_type == OptType.naive:
         max_sum = naive(params.lists, params.m, params.f)
     else:
         max_sum = efficient(params.lists, params.m, params.f)
--- src/utils/modulo_maximisation.py
+++ src/utils/modulo_maximisation.py
@@ -1,47 +1,74 @@
+import itertools
+import multiprocessing
+import os
+from concurrent.futures import ProcessPoolExecutor, as_completed
 from typing import List, Callable
+from .optimisations import slightly_efficient
+
+
+MAX_K = int(os.getenv("MAX_K", 5))
 
 
 def naive(lists: List[List[int]], m: int, f: Callable) -> int:
     """
-    :param lists: List of lists with values to choose from.
-    :param m: Quotient of modulo operator.
-    :param f: Function to map x to f.
-    :return: Maximum
-    """
-    f_res = [[] for i in range(len(lists))]
+    This function has a complexity of O(n ^ (m*k)), where `k` is the number of lists, `n` is the sum of sizes of lists and
+    `m` is the factor of the total number of combinations by selecting one element from each list.
+    This function computes all possible combinations of elements by selecting one value from each nested list.
+    It then computes the maximum value of the modulo function from all combinations.
 
-    for i, sub_list in enumerate(lists):
-        for j in range(len(sub_list)):
-            if i == 0:
-                f_res[i].append(f(sub_list[j]) % m)
-            else:
-                for k in range(len(f_res[i - 1])):
-                    value = f_res[i - 1][k] + f(sub_list[j])
-                    f_res[i].append(value % m)
-    return max(f_res[-1])
+            Parameters:
+                    lists (List[List[int]]):List of lists with values to choose from
+                    m (int): Quotient of modulo operator
+                    f (Callable): Function to map x to f
+
+            Returns:
+                    max_sum (int): Maximised value of the `∑(f(x)) % m` where x is one value from each nested list in lists
+    """
+    all_possible_combinations = itertools.product(*lists)
+    max_sum = 0
+    for combination in all_possible_combinations:
+        local_sum = 0
+        for num in combination:
+            local_sum = local_sum + f(num)
+        local_sum = local_sum % m
+        if local_sum > max_sum:
+            max_sum = local_sum
+    return max_sum
 
 
 def efficient(lists: List[List[int]], m: int, f: Callable) -> int:
     """
-    :param lists: List of lists with values to choose from.
-    :param m: Quotient of modulo operator.
-    :param f: Function to map x to f.
-    :return: Maximum
+    This function has a complexity of O(n ^ k), where `k` is the number of lists and `n` is the sum of sizes of lists.
+    The function computes the maximum value of the modulo function during each iteration, by leveraging the
+    distributive property of the modulo function `(a + b) mod n = [(a mod n) + (b mod n)] mod n`.
+
+    This also allows the function to be parallelised to achive better computational efficiency for higher values of `k`.
+
+            Parameters:
+                    lists (List[List[int]]):List of lists with values to choose from
+                    m (int): Quotient of modulo operator
+                    f (Callable): Function to map x to f
+
+            Returns:
+                    max_sum (int): Maximised value of the `∑(f(x)) % m` where x is one value from each nested list in lists
     """
-    f_res = [[] for i in range(len(lists))]
     max_sum = 0
-    for i, sub_list in enumerate(lists):
-        for j in range(len(sub_list)):
-            if i == 0:
-                cur_sum = f(sub_list[j]) % m
-                f_res[i].append(cur_sum)
-                if cur_sum > max_sum:
-                    max_sum = cur_sum
-            else:
-                for k in range(len(f_res[i - 1])):
-                    value = f_res[i - 1][k] + f(sub_list[j])
-                    cur_sum = value % m
-                    f_res[i].append(cur_sum)
-                    if cur_sum > max_sum:
-                        max_sum = cur_sum
+
+    def split(target: List):
+        for i in range(0, len(target), MAX_K):
+            yield target[i : i + MAX_K]
+
+    while len(lists) > MAX_K:
+        combined_batch_futures = []
+        combined_batch_res = []
+        with ProcessPoolExecutor(max_workers=multiprocessing.cpu_count() - 2) as executor:
+            for batch in split(lists):
+                future = executor.submit(slightly_efficient, batch, m, f)
+                combined_batch_futures.append(future)
+            for res in as_completed(combined_batch_futures):
+                _, subset_res = res.result()
+                combined_batch_res.append(subset_res)
+        lists = combined_batch_res
+
+    max_sum, _ = slightly_efficient(lists, m, f)
     return max_sum
